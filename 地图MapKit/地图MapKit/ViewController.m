76aaaccc7afdcaaef39817ebf9e6adc2d0d0b4fe
zhangli <313890735@qq.com>
2016-02-19 21:00:35 +0800

    定位用户位置

--- 地图MapKit/地图MapKit/ViewController.m
+++ 地图MapKit/地图MapKit/ViewController.m
@@ -12,7 +12,7 @@
 #import <CoreLocation/CoreLocation.h>
 
 
-@interface ViewController ()
+@interface ViewController ()<MKMapViewDelegate>
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property(nonatomic,strong)CLLocationManager * locationM ;
 @end
@@ -27,11 +27,11 @@
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
 //    self.mapView.mapType = MKMapTypeHybridFlyover ;
 //    self.mapView.zoomEnabled = NO ;
-    self.mapView.showsCompass = NO;
+//    self.mapView.showsCompass = NO;
     //比例尺
     self.mapView.showsScale = YES ;
     //显示建筑物
-    self.mapView.showsBuildings = YES ;
+//    self.mapView.showsBuildings = YES ;
     [self locationM];
     //请求获取用户位置，需要授权（导入CoreLocation框架）
     self.mapView.showsUserLocation = YES ;
@@ -39,6 +39,28 @@
     self.mapView.userTrackingMode = MKUserTrackingModeFollowWithHeading ;
 }
 
+#pragma  mark - MKMapViewDelegate
+- (void) mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+      /**
+        MKUserLocation (大头针模型)
+       */
+    userLocation.title = @"你猜你猜你猜猜猜";
+    userLocation.subtitle = @"去找百度地图";
+   
+    //设置地图显示中心
+    [self.mapView setCenterCoordinate:userLocation.location.coordinate animated:YES];
+    
+    //设置地图显示区域
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.051109, 0.034153);
+    MKCoordinateRegion region = MKCoordinateRegionMake(userLocation.location.coordinate, span);
+    [self.mapView setRegion:region animated:YES];
+    
+}
+
+- (void) mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated
+{
+   NSLog(@"%f----%f", mapView.region.span.latitudeDelta, mapView.region.span.longitudeDelta);
+}
 
 - (CLLocationManager *)locationM
 {
@@ -52,6 +74,8 @@
     }
     return _locationM ;
 }
+
+
 @end
 
 
